ab5ddf58685e03b3c8e4663e3ae7d2f46c11378a
henry <henry@henrydeMacBook-Pro.local>
2025-12-12 08:26:33 +0800

    feat：修改客服

--- src/store/modules/chat.ts
+++ src/store/modules/chat.ts
@@ -1,60 +1,18 @@
-import { store } from "@/store";
-import { getLanguage } from "@/i18n";
-import { kkAuth } from "@/01-kk-system/allUtils/kkAuth";
-import { platformHttp } from "@/01-kk-system/allHttp/userHall/platform";
-
-type ContactInfo = {
-  name?: string;
-  email?: string;
-  phone?: string;
-  description?: string;
-  userID?: string;
-};
+import { defineStore } from 'pinia'
+import { store } from '@/store'
+import { type ContactInfo, useKKTalk } from '@/04-kk-component-admin/components/hooks/useKKTalk'
 
 // setup
-export const useChatStore = defineStore("chat", () => {
-  // const chatUrl = "//code.jivosite.com/widget/hqVvZCsJiG";
-  const chatBaseUrl = "//code.jivosite.com/widget/";
-  const scriptID = "JIVOChat-Script";
-  let chatLoaded = false;
-  let chatConfig: Record<string, any> = {};
+export const useChatStore = defineStore('chat', () => {
+  const { loadChat: loadKKTalkChat, openChat: openKKTalkChat, setCustomData: setKKTalkCustomData } = useKKTalk()
 
   /**
    * 加载chat
+   * @param hideWidget 是否隐藏客服小窗口
    * @param chatCustomUrl
    */
-  async function loadChat(chatCustomUrl?: string) {
-    const res = await platformHttp.getChatConfig();
-    const { data } = res || {};
-    if (data) {
-      chatConfig = data;
-    }
-    removeScript();
-    if (!chatConfig.jivochatID) {
-      return;
-    }
-    let url = chatCustomUrl || `${chatBaseUrl}${chatConfig.jivochatID}`;
-    url = url.includes("//") ? url : "//" + url;
-    let head = document.getElementsByTagName("head")[0];
-    let script = document.createElement("script");
-    script.type = "text/javascript";
-    script.defer = true;
-    script.id = scriptID;
-    const lang = getLanguage();
-    script.src = url + "?locale=" + getLanguage;
-    script.onload = () => {
-      chatLoaded = true;
-    };
-    head.appendChild(script);
-  }
-  /**
-   * 移除chat脚本
-   */
-  function removeScript() {
-    let script = document.getElementById(scriptID);
-    let ukefu = document.getElementById("jivo-iframe-container");
-    script && document.head.removeChild(script);
-    ukefu && document.body.removeChild(ukefu);
+  async function loadChat(hideWidget?: boolean, chatCustomUrl?: string) {
+    loadKKTalkChat(hideWidget, chatCustomUrl)
   }
   /**
    * 配置信息
@@ -64,64 +22,32 @@ export const useChatStore = defineStore("chat", () => {
    * @param description
    */
   function setContactInfo({
-    name = "",
-    email = "",
-    phone = "",
-    description = "",
-    userID = "",
+    name = '',
+    email = '',
+    phone = '',
+    description = '',
+    userID = '',
   }: ContactInfo) {
-    // @ts-ignore
-    if (window.jivo_api) {
-      // @ts-ignore
-      window.jivo_api.setContactInfo({
-        name,
-        email,
-        phone,
-        description,
-        userID,
-      });
-    }
+    setKKTalkCustomData({
+      name,
+      email,
+      phone,
+      description,
+      userID,
+    })
   }
   /**
    * 设置客户信息
    * @param data
    */
-  function setCustomData(data: any[]) {
-    // @ts-ignore
-    window.jivo_api && window.jivo_api.setCustomData(data);
+  function setCustomData(data: ContactInfo) {
+    setKKTalkCustomData(data)
   }
   /**
    * 打开客服
    */
   function openChat() {
-    const userInfo = kkAuth.getUserInfo();
-    const {
-      userName = "",
-      nickName = "",
-      platformID = "",
-      userID = "",
-    } = userInfo || {};
-    setContactInfo({ name: nickName });
-    setCustomData([
-      {
-        title: "用户名：",
-        content: userName,
-      },
-      {
-        title: "用户ID",
-        content: userID,
-      },
-      {
-        title: "平台ID",
-        content: platformID,
-      },
-    ]);
-    // @ts-ignore
-    if (window?.jivo_api?.open) {
-      console.log("opened");
-      // @ts-ignore
-      window.jivo_api.open();
-    }
+    openKKTalkChat()
   }
 
   return {
@@ -129,10 +55,10 @@ export const useChatStore = defineStore("chat", () => {
     setContactInfo,
     openChat,
     setCustomData,
-  };
-});
+  }
+})
 
 // 手动提供给 useStore() 函数 pinia 实例
 export function useChatStoreHook() {
-  return useChatStore(store);
+  return useChatStore(store)
 }
